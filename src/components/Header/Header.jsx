d10f88e071e9f8dc9271dfa264d542be62f2906f
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-02-27 04:25:21 -0500

    header.jsx update image.logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__user-name"></p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
