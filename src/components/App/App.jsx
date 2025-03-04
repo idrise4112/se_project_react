c6e27350fdfaee960fa26f8223156f7c5a414705
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-03-03 21:54:59 -0500

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import WeatherCard from "../WeatherCard/WeatherCard";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,21 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
       <section className="cards">
         <p className="card__text">Today is 75&deg; F / You may want to wear:</p>
         <ul className="cards__list">
-          {defaultClothingItems.map((item) => {
-            return (
-              <div key={item._id}>
-                <h2>{item.name}</h2>
-                <img src={item.link} alt={item.name} />
-              </div>
-            );
-          })}
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
         </ul>
       </section>
     </main>
