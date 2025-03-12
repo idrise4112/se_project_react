ba9a7207184d2d786a5117a2f9bebf208955b83d
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-03-12 11:24:35 -0400

    new update

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,22 @@
-import sunny from "../../assets/sunny.png";
 import "./WeatherCard.css";
+import { weatherOptions } from "../../utils/constants";
+
 function WeatherCard({ weatherData }) {
+  const weatherOption = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
-      <img src={sunny} alt="sunny" className="weather-card__image" />
+      <p className="weather-card__temp">{weatherData.temp_F} &deg; F</p>
+      <img
+        src={weatherOption.url}
+        alt="sunny"
+        className="weather-card__image"
+      />
     </section>
   );
 }
