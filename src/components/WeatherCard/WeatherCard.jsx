4b62252fcf64c838d4b26033654d766556645089
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-03-15 23:41:27 -0400

    new update

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,20 +1,30 @@
 import "./WeatherCard.css";
-import { weatherOptions } from "../../utils/constants";
+import { defaultWeatherOptions, weatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
-  const weatherOption = weatherOptions.filter((option) => {
+  const filteredOptions = weatherOptions.filter((option) => {
     return (
       option.day === weatherData.isDay &&
       option.condition === weatherData.condition
     );
   });
 
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption =
+      defaultWeatherOptions[weatherData.isNight ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp_F} &deg; F</p>
+      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
       <img
-        src={weatherOption.url}
-        alt="sunny"
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherOption?.day ? "day" : "night"} time ${
+          weatherOption?.condition
+        } weather`}
         className="weather-card__image"
       />
     </section>
