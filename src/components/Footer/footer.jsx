fac523435d063ab6032216fc5bf38c7c99b3998a
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-03-23 00:57:00 -0400

    new update

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__text">Developed by Idris Ellams 2025.</p>
+        <div className="footer__links">{}</div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -30,6 +30,26 @@ export const weatherOptions = [
     url: new URL("../assets/day/snow.png", import.meta.url).href,
   },
   {
+    day: true,
+    condition: "storm",
+    url: new URL("../assets/day/storm.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snow.png", import.meta.url).href,
+  },
+  {
     day: false,
     condition: "storm",
     url: new URL("../assets/night/storm.png", import.meta.url).href,
