19629583e22acf48c0ab3be39147a7c4bd862eb0
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-03-24 15:17:10 -0400

    new update

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -8,9 +8,11 @@ function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
     >
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button onClick={onClose} type="button" className="modal__close">
-          CLOSE
-        </button>
+        <button
+          onClick={onClose}
+          type="button"
+          className="modal__close"
+        ></button>
         <form className="modal__form">
           {children}
           <button type="submit" className="modal__submit">
