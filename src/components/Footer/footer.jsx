03631b38a92aaa212b94a48d9d45e85295e8e984
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-03-25 17:42:41 -0400

    remove node modules

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,10 +2,8 @@ import "./footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__content">
-        <p className="footer__text">Developed by Idris Ellams </p>
-        <h1 className="footer__year">2025</h1>
-      </div>
+      <p className="footer__text">Developed by Idris Ellams </p>
+      <h1 className="footer__year">2025</h1>
     </footer>
   );
 }
