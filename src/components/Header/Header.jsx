b4f1653d7f2ba48dca546af1313409f5d3416a5d
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-04-23 12:35:48 -0400

    toggle switch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, city }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -15,6 +16,7 @@ function Header({ handleAddClick, city }) {
         {currentDate}, {city || "Loading..."}
       </p>
       <div className="header__actions">
+        <ToggleSwitch />
         <button
           className="header__button"
           onClick={handleAddClick}
---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,9 @@
+import "./ToggleSwitch.css";
+
+export default function ToggleSwitch() {
+  return (
+    <label className="toggle-switch">
+      <input type="text" className="toggle-switch__checkbox" />
+    </label>
+  );
+}
